5ad84c5afdfffec1d396264514b263370ca07f63
maciej simka <mcj.simka@gmail.com>
2020-02-17 22:27:26 +0100

    feat: add react rules

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json"
+const common = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["react", "react-hooks"]
+};
+
+module.exports = {
+  env: common.env,
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react"
   ],
-  rules: {}
+  globals: {},
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: common.plugins,
+  rules: common.rules,
+  settings: { react: { version: "detect" } },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      env: common.env,
+      extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/react"
+      ],
+      globals: {},
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2018,
+        sourceType: "module",
+        project: "./tsconfig.json"
+      },
+      plugins: ["@typescript-eslint", ...common.plugins],
+      settings: { react: { version: "detect" } }
+    }
+  ],
+  rules: {
+    "react-hooks/rules-of-hooks": "error"
+  }
 };
