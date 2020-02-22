6f55b1c3d17359f79753ba5fee067602a3a81965
maciej simka <mcj.simka@gmail.com>
2020-02-22 21:54:43 +0100

    feat: add more plugins

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const common = {
+"use strict";
+
+const restrictedGlobals = require("confusing-browser-globals");
+
+module.exports = {
+  root: true,
+
+  parser: "babel-eslint",
+
+  plugins: [
+    "import",
+    "jsx-a11y",
+    "react",
+    "react-hooks",
+    "eslint-plugin-unicorn"
+  ],
+
   env: {
     browser: true,
     commonjs: true,
@@ -6,31 +22,43 @@ const common = {
     jest: true,
     node: true
   },
-  plugins: ["react", "react-hooks"]
-};
 
-module.exports = {
-  env: common.env,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "prettier",
     "prettier/react"
   ],
-  globals: {},
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaFeatures: { jsx: true },
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  plugins: common.plugins,
-  rules: common.rules,
-  settings: { react: { version: "detect" } },
+
   overrides: [
     {
-      files: ["**/*.ts", "**/*.tsx"],
-      env: common.env,
+      files: ["**/*.ts?(x)"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        },
+
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true
+      },
+      plugins: ["@typescript-eslint"],
       extends: [
         "eslint:recommended",
         "plugin:react/recommended",
@@ -40,19 +68,60 @@ module.exports = {
         "prettier/@typescript-eslint",
         "prettier/react"
       ],
-      globals: {},
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 2018,
-        sourceType: "module",
-        project: "./tsconfig.json"
-      },
-      plugins: ["@typescript-eslint", ...common.plugins],
-      settings: { react: { version: "detect" } }
+      rules: {
+        // TypeScript specific rules (remember to turn off ESLint for those)
+      }
     }
   ],
+
   rules: {
-    "react-hooks/rules-of-hooks": "error"
+    "react-hooks/rules-of-hooks": "error",
+    "unicorn/better-regex": "error",
+    "unicorn/catch-error-name": "error",
+    "unicorn/consistent-function-scoping": "error",
+    "unicorn/custom-error-definition": "off",
+    "unicorn/error-message": "error",
+    "unicorn/escape-case": "error",
+    "unicorn/expiring-todo-comments": "error",
+    "unicorn/explicit-length-check": "error",
+    "unicorn/filename-case": "error",
+    "unicorn/import-index": "error",
+    "unicorn/new-for-builtins": "error",
+    "unicorn/no-abusive-eslint-disable": "error",
+    "unicorn/no-array-instanceof": "error",
+    "unicorn/no-console-spaces": "error",
+    "unicorn/no-fn-reference-in-iterator": "off",
+    "unicorn/no-for-loop": "error",
+    "unicorn/no-hex-escape": "error",
+    "unicorn/no-keyword-prefix": "off",
+    "no-nested-ternary": "off",
+    "unicorn/no-nested-ternary": "error",
+    "unicorn/no-new-buffer": "error",
+    "unicorn/no-process-exit": "error",
+    "unicorn/no-unreadable-array-destructuring": "error",
+    "unicorn/no-unsafe-regex": "off",
+    "unicorn/no-unused-properties": "off",
+    "unicorn/no-zero-fractions": "error",
+    "unicorn/number-literal-case": "error",
+    "unicorn/prefer-add-event-listener": "error",
+    "unicorn/prefer-dataset": "error",
+    "unicorn/prefer-event-key": "error",
+    "unicorn/prefer-flat-map": "error",
+    "unicorn/prefer-includes": "error",
+    "unicorn/prefer-modern-dom-apis": "error",
+    "unicorn/prefer-negative-index": "error",
+    "unicorn/prefer-node-append": "error",
+    "unicorn/prefer-node-remove": "error",
+    "unicorn/prefer-query-selector": "error",
+    "unicorn/prefer-reflect-apply": "error",
+    "unicorn/prefer-replace-all": "off",
+    "unicorn/prefer-spread": "error",
+    "unicorn/prefer-starts-ends-with": "error",
+    "unicorn/prefer-string-slice": "error",
+    "unicorn/prefer-text-content": "error",
+    "unicorn/prefer-trim-start-end": "error",
+    "unicorn/prefer-type-error": "error",
+    "unicorn/prevent-abbreviations": "error",
+    "unicorn/throw-new-error": "error"
   }
 };
