1fae1b3ac804dd45294cab436abaf4309467f999
maciej simka <mcj.simka@gmail.com>
2020-02-23 09:01:03 +0100

    feat(a11y/imports): add new rules

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const restrictedGlobals = require("confusing-browser-globals");
 
 module.exports = {
@@ -75,7 +73,36 @@ module.exports = {
   ],
 
   rules: {
+    "import/first": "error",
+    "import/no-amd": "error",
+    "import/no-webpack-loader-syntax": "error",
+
+    "jsx-a11y/accessible-emoji": "warn",
+    "jsx-a11y/alt-text": "warn",
+    "jsx-a11y/anchor-has-content": "warn",
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        aspects: ["noHref", "invalidHref"]
+      }
+    ],
+    "jsx-a11y/aria-activedescendant-has-tabindex": "warn",
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-role": ["warn", { ignoreNonDOM: true }],
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/heading-has-content": "warn",
+    "jsx-a11y/iframe-has-title": "warn",
+    "jsx-a11y/img-redundant-alt": "warn",
+    "jsx-a11y/no-access-key": "warn",
+    "jsx-a11y/no-distracting-elements": "warn",
+    "jsx-a11y/no-redundant-roles": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+    "jsx-a11y/scope": "warn",
+
     "react-hooks/rules-of-hooks": "error",
+
     "unicorn/better-regex": "error",
     "unicorn/catch-error-name": "error",
     "unicorn/consistent-function-scoping": "error",
